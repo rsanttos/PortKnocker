99608d28942614cd24dfe257c49050a6140029eb
Ramon Santos <rsanttos22@gmail.com>
2017-11-15 20:27:00 -0300

    Organização do código

--- src/br/com/ufrn/protocolos/dns/portknocker/main/Cliente.java
+++ src/br/com/ufrn/protocolos/dns/portknocker/main/Cliente.java
@@ -4,53 +4,38 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class Cliente {
-	
+
 	static Scanner leitorTeclado = new Scanner(System.in);
+
 	public static void main(String[] args) throws IOException {
 
-		Socket socket = new Socket("localhost", 7777);
-		System.out.println("Cliente 1 conectado com o servidor " + socket.getInetAddress().getHostAddress() + ":"
+		int[] portas = { 6666, 7777, 8888, 9999 };
+		String host = "localhost";
+
+		for (int i = 0; i < portas.length; i++) {
+			try {
+				realizaConexao(host, portas[i]);
+			} catch (Exception e) {
+				System.out.println("Porta " + portas[i] + " inválida.");
+			}
+		}
+	}
+
+	public static void realizaConexao(String host, int porta) throws UnknownHostException, IOException {
+		Socket socket = new Socket(host, porta);
+		System.out.println("Cliente conectado com o servidor " + socket.getInetAddress().getHostAddress() + ":"
 				+ socket.getPort());
 
-		DataOutputStream outBytes = new DataOutputStream(socket.getOutputStream());
-		String mensagem = "";
-
-		System.out.print("Digite a mensagem para enviar ao servidor: ");
-		mensagem = leitorTeclado.nextLine();		
-		
-		outBytes.write(mensagem.getBytes());
-		
 		DataInputStream inBytes = new DataInputStream(socket.getInputStream());
 		byte[] data = new byte[128];
 		inBytes.read(data);
 		String mensagemRecebida = new String(data).trim();
-		System.out.println("Resposta do servidor: " + mensagemRecebida);			
-		
+		System.out.println("Resposta do servidor: " + mensagemRecebida);
+
 		socket.close();
-		
-		/* --------------------------- */
-		
-		Socket socket2 = new Socket("localhost", 8888);
-		System.out.println("Cliente 2 conectado com o servidor " + socket2.getInetAddress().getHostAddress() + ":"
-				+ socket2.getPort());
-
-		DataOutputStream outBytes2 = new DataOutputStream(socket2.getOutputStream());
-		String mensagem2 = "";
-
-		System.out.print("Digite a mensagem para enviar ao servidor: ");
-		mensagem2 = leitorTeclado.nextLine();		
-		
-		outBytes2.write(mensagem2.getBytes());
-		
-		DataInputStream inBytes2 = new DataInputStream(socket2.getInputStream());
-		byte[] data2 = new byte[128];
-		inBytes2.read(data2);
-		String mensagemRecebida2 = new String(data2).trim();
-		System.out.println("Resposta do servidor: " + mensagemRecebida2);			
-		
-		socket2.close();
 	}
 }
--- src/br/com/ufrn/protocolos/dns/portknocker/main/Servidor.java
+++ src/br/com/ufrn/protocolos/dns/portknocker/main/Servidor.java
@@ -8,48 +8,31 @@ import java.net.Socket;
 
 public class Servidor {
 	public static void main(String[] args) throws IOException {
+		int[] portas = { 6666, 7777, 8888, 9998 };
 
-		System.out.println("Servidor em execu��o.");
-		while (true) {			
-			ServerSocket serverSocket1 = new ServerSocket(7777);
-			System.out.println("--------------------------------------------------------------------");
-			System.out.println("Aguardando conexão do cliente no serverSocket1...");
-			Socket socket1 = serverSocket1.accept();
-			System.out.println("Cliente " + socket1.getInetAddress().getHostAddress() + " conectado!");
-			DataInputStream inBytes = new DataInputStream(socket1.getInputStream());
-			byte[] data = new byte[128];
-			inBytes.read(data);
-			
-			String mensagemRecebida = new String(data).trim();
-			System.out.println("Mensagem recebida socket1: " + mensagemRecebida);			
-			DataOutputStream outBytes = new DataOutputStream(socket1.getOutputStream());				
-			outBytes.write(mensagemRecebida.getBytes());			
-			System.out.println("Mensagem retornada do socket1 para o cliente: " + mensagemRecebida);
-			
-			
-			socket1.close();
-			serverSocket1.close();
-			System.out.println("--------------------------------------------------------------------");
-			
-			ServerSocket serverSocket2 = new ServerSocket(8888);
-			System.out.println("--------------------------------------------------------------------");
-			System.out.println("Aguardando conexão do cliente no serverSocket2...");
-			Socket socket2 = serverSocket2.accept();
-			System.out.println("Cliente " + socket2.getInetAddress().getHostAddress() + " conectado!");
-			DataInputStream inBytes2 = new DataInputStream(socket2.getInputStream());
-			byte[] data2 = new byte[128];
-			inBytes2.read(data2);
-			
-			String mensagemRecebida2 = new String(data2).trim();
-			System.out.println("Mensagem recebida socket2: " + mensagemRecebida2);			
-			DataOutputStream outBytes2 = new DataOutputStream(socket2.getOutputStream());				
-			outBytes.write(mensagemRecebida2.getBytes());			
-			System.out.println("Mensagem retornada do socket2 para o cliente: " + mensagemRecebida2);
-			
-			
-			System.out.println("--------------------------------------------------------------------");
-			socket2.close();
-			serverSocket2.close();
+		System.out.println("Servidor em execução.");
+		
+		for (int i = 0; i < portas.length; i++) {
+			try {
+				recebeConexao(portas[i]);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
+	}
+	
+	public static void recebeConexao(int porta) throws IOException {
+		ServerSocket serverSocket = new ServerSocket(porta);
+		System.out.println("--------------------------------------------------------------------");
+		System.out.println("Aguardando conexão do cliente no servidor. Porta: " + porta);
+		Socket socket = serverSocket.accept();
+		System.out.println("Cliente " + socket.getInetAddress().getHostAddress() + " conectado.");			
+		DataOutputStream outBytes = new DataOutputStream(socket.getOutputStream());		
+		String mensagemResposta = "CONEXAO_OK";
+		outBytes.write(mensagemResposta.getBytes());			
+		System.out.println("Mensagem retornada do socket1 para o cliente: " + mensagemResposta);			
+		socket.close();
+		serverSocket.close();
+		System.out.println("--------------------------------------------------------------------");
 	}
 }
