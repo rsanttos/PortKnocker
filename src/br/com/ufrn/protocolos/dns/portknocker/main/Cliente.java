ffc63233b8b81146db02c0af6a955e5b373db750
Ramon Santos <rsanttos22@gmail.com>
2017-11-16 19:05:15 -0300

    Finalização da primeira versão do projeto

--- src/br/com/ufrn/protocolos/dns/portknocker/main/Cliente.java
+++ src/br/com/ufrn/protocolos/dns/portknocker/main/Cliente.java
@@ -1,7 +1,6 @@
 package br.com.ufrn.protocolos.dns.portknocker.main;
 
 import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -11,16 +10,18 @@ public class Cliente {
 
 	static Scanner leitorTeclado = new Scanner(System.in);
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException {
 
 		int[] portas = { 6666, 7777, 8888, 9999 };
 		String host = "localhost";
-
+		
 		for (int i = 0; i < portas.length; i++) {
 			try {
 				realizaConexao(host, portas[i]);
 			} catch (Exception e) {
 				System.out.println("Porta " + portas[i] + " inválida.");
+				e.printStackTrace();
+				Thread.sleep(10000);
 			}
 		}
 	}
@@ -29,13 +30,19 @@ public class Cliente {
 		Socket socket = new Socket(host, porta);
 		System.out.println("Cliente conectado com o servidor " + socket.getInetAddress().getHostAddress() + ":"
 				+ socket.getPort());
-
 		DataInputStream inBytes = new DataInputStream(socket.getInputStream());
 		byte[] data = new byte[128];
 		inBytes.read(data);
 		String mensagemRecebida = new String(data).trim();
 		System.out.println("Resposta do servidor: " + mensagemRecebida);
-
 		socket.close();
+		
+		int posicao = mensagemRecebida.indexOf("=");
+		if(posicao > 0) {
+			String portaPrincipal = mensagemRecebida.substring(posicao + 1, mensagemRecebida.length());
+			int portaPrincipalInt = Integer.parseInt(portaPrincipal);
+			realizaConexao(host, portaPrincipalInt);
+		}
+		
 	}
 }
--- src/br/com/ufrn/protocolos/dns/portknocker/main/Servidor.java
+++ src/br/com/ufrn/protocolos/dns/portknocker/main/Servidor.java
@@ -1,6 +1,5 @@
 package br.com.ufrn.protocolos.dns.portknocker.main;
 
-import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -8,31 +7,58 @@ import java.net.Socket;
 
 public class Servidor {
 	public static void main(String[] args) throws IOException {
-		int[] portas = { 6666, 7777, 8888, 9998 };
+		int[] portas = { 6666, 7777, 8888, 9999 };
 
 		System.out.println("Servidor em execução.");
-		
-		for (int i = 0; i < portas.length; i++) {
-			try {
-				recebeConexao(portas[i]);
-			} catch (Exception e) {
-				e.printStackTrace();
+		String enderecoCliente = "enderecoCliente"; 
+		String enderecoClienteAnterior = "";
+
+		while (true) {
+			int i = 0;
+			while (i < portas.length) {
+				try {
+					enderecoCliente = recebeConexao(portas[i], i, portas.length);
+					if (i > 0 && !enderecoClienteAnterior.equals(enderecoCliente)) {
+						return;
+					}
+
+					if (i == 0 && !enderecoClienteAnterior.equals(enderecoCliente)) {
+						enderecoClienteAnterior = enderecoCliente;
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				i++;
 			}
+
 		}
 	}
-	
-	public static void recebeConexao(int porta) throws IOException {
+
+	public static String recebeConexao(int porta, int posicao, int qtdPortas) throws IOException {
 		ServerSocket serverSocket = new ServerSocket(porta);
 		System.out.println("--------------------------------------------------------------------");
 		System.out.println("Aguardando conexão do cliente no servidor. Porta: " + porta);
+		//serverSocket.setSoTimeout(9000);
 		Socket socket = serverSocket.accept();
-		System.out.println("Cliente " + socket.getInetAddress().getHostAddress() + " conectado.");			
-		DataOutputStream outBytes = new DataOutputStream(socket.getOutputStream());		
-		String mensagemResposta = "CONEXAO_OK";
-		outBytes.write(mensagemResposta.getBytes());			
-		System.out.println("Mensagem retornada do socket1 para o cliente: " + mensagemResposta);			
+		String enderecoCliente = socket.getInetAddress().getHostAddress();
+		System.out.println("Cliente " + enderecoCliente + " conectado.");
+		DataOutputStream outBytes = new DataOutputStream(socket.getOutputStream());
+		String mensagemResposta = "";
+		if(posicao+1 == qtdPortas) {
+			mensagemResposta = "CONEXAO_OK -> PORTA PRINCIPAL=5555";
+		} else {
+			mensagemResposta = "CONEXAO_OK";
+		}
+		outBytes.write(mensagemResposta.getBytes());
+		System.out.println("Mensagem retornada do socket1 para o cliente: " + mensagemResposta);
 		socket.close();
 		serverSocket.close();
 		System.out.println("--------------------------------------------------------------------");
+		
+		if(mensagemResposta.equals("CONEXAO_OK -> PORTA PRINCIPAL=5555")) {
+			return recebeConexao(5555, 0, 5);
+		}
+		
+		return enderecoCliente;
 	}
 }
